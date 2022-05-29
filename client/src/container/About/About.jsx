65720868d680824ba8b2aa5593b387a628a3830f
DivineDemon <supame123@gmail.com>
2022-05-29 23:45:42 +0400

    Skills Section Done!

--- client/src/container/About/About.jsx
+++ client/src/container/About/About.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
+
+import { AppWrap, MotionWrap } from "../../wrapper";
 import "./About.scss";
 import { urlFor, client } from "../../client";
-import { AppWrap } from "../../wrapper";
 
 const About = () => {
   const [abouts, setAbouts] = useState([]);
@@ -18,7 +19,7 @@ const About = () => {
   return (
     <>
       <h2 className="head-text">
-        I Know that <span>Good Apps</span> <br />
+        I Know that <span>Good Design</span> <br />
         means <span>Good Business</span>
       </h2>
 
@@ -45,4 +46,8 @@ const About = () => {
   );
 };
 
-export default AppWrap(About, "about");
+export default AppWrap(
+  MotionWrap(About, "app__about"),
+  "about",
+  "app__whitebg"
+);
